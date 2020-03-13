cc835e583d74263c32ac32f89e1e6787e80230fb
ally0426 <abinding@gmail.com>
2020-03-12 19:41:49 -0500

    first trial

---
+++ client/src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1 class="display3">About Us</h1>
+    <div class="card p-3">
+      <h4>Vue-team-17</h4>
+      <p>This is an app to experiment to create online meetings with people in different timezones.</p>
+    </div>
+  </div>
+</template>
